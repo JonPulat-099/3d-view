0c2d3f17133868cdec8e26ec9ea4e62309f7ad7d
JonPulat-099 <rjpm023@gmail.com>
2024-05-30 18:59:03 +0500

    remove zoom & change background (add RoomEnviroment)

--- src/App.vue
+++ src/App.vue
@@ -5,44 +5,31 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader'
 import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment'
+
 import { onMounted, onUnmounted } from 'vue'
 
-let camera, scene, renderer
+let camera, scene, renderer, controls
 
 function init() {
   const container = document.createElement('div')
   document.body.appendChild(container)
 
   camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 20)
-  camera.position.set( - 1.8, 0.6, 2.7 );
+  camera.position.set(-1.8, 0.6, 2.7)
 
   scene = new THREE.Scene()
 
-  new RGBELoader()
-    .setPath('textures/equirectangular/')
-    .load('royal_esplanade_1k.hdr', function (texture) {
-      texture.mapping = THREE.EquirectangularReflectionMapping
+  new GLTFLoader().setPath('models/v2/').load('02.gltf', async function (gltf) {
+    const model = gltf.scene
 
-      scene.background = texture
-      scene.environment = texture
+    // wait until the model can be added to the scene without blocking due to shader compilation
 
-      render()
+    await renderer.compileAsync(model, camera, scene)
 
-      // model
+    scene.add(model)
 
-      const loader = new GLTFLoader().setPath('models/v2/')
-      loader.load('02.gltf', async function (gltf) {
-        const model = gltf.scene
-
-        // wait until the model can be added to the scene without blocking due to shader compilation
-
-        await renderer.compileAsync(model, camera, scene)
-
-        scene.add(model)
-
-        render()
-      })
-    })
+    render()
+  })
 
   renderer = new THREE.WebGLRenderer({ antialias: true })
   renderer.setPixelRatio(window.devicePixelRatio)
@@ -51,10 +38,16 @@ function init() {
   renderer.toneMappingExposure = 1
   container.appendChild(renderer.domElement)
 
-  const controls = new OrbitControls(camera, renderer.domElement)
+  const environment = new RoomEnvironment(renderer)
+  const pmremGenerator = new THREE.PMREMGenerator(renderer)
+
+  scene.background = new THREE.Color(0xbbbbbb)
+  scene.environment = pmremGenerator.fromScene(environment).texture
+
+  controls = new OrbitControls(camera, renderer.domElement)
   controls.addEventListener('change', render) // use if there is no animation loop
-  controls.minDistance = 2
-  controls.maxDistance = 10
+  controls.minDistance = 3
+  controls.maxDistance = 3
   controls.target.set(0, 0, -0.2)
   controls.update()
 
