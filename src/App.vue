5785c11e5f0828e114f91964ad4c8d11dab57166
JonPulat-099 <rjpm023@gmail.com>
2024-05-31 19:15:23 +0500

    change: touch events

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,30 @@ import * as THREE from 'three'
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader'
 import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment'
 
 import { onMounted, onUnmounted } from 'vue'
 
-let camera, scene, renderer, controls
+let camera,
+  scene,
+  renderer,
+  controls,
+  canvas,
+  model,
+  isTouch = false,
+  positionY,
+  actionID
 
 function init() {
-  const container = document.createElement('div')
-  document.body.appendChild(container)
+  canvas = document.getElementById('card')
 
-  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 20)
-  camera.position.set(-1.8, 0.6, 2.7)
+  camera = new THREE.PerspectiveCamera(55, 3 / 2, 0.25, 20)
+  camera.position.set(0, 0, 2)
 
   scene = new THREE.Scene()
 
   new GLTFLoader().setPath('models/v2/').load('02.gltf', async function (gltf) {
-    const model = gltf.scene
+    model = gltf.scene
 
     // wait until the model can be added to the scene without blocking due to shader compilation
 
@@ -31,12 +37,11 @@ function init() {
     render()
   })
 
-  renderer = new THREE.WebGLRenderer({ antialias: true })
+  renderer = new THREE.WebGLRenderer({ antialias: true, canvas })
   renderer.setPixelRatio(window.devicePixelRatio)
-  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setSize(window.innerWidth, window.innerWidth * 0.67)
   renderer.toneMapping = THREE.ACESFilmicToneMapping
   renderer.toneMappingExposure = 1
-  container.appendChild(renderer.domElement)
 
   const environment = new RoomEnvironment(renderer)
   const pmremGenerator = new THREE.PMREMGenerator(renderer)
@@ -44,31 +49,60 @@ function init() {
   scene.background = new THREE.Color(0xbbbbbb)
   scene.environment = pmremGenerator.fromScene(environment).texture
 
-  controls = new OrbitControls(camera, renderer.domElement)
+  controls = new OrbitControls(camera, canvas)
   controls.addEventListener('change', render) // use if there is no animation loop
   controls.minDistance = 3
   controls.maxDistance = 3
-  controls.target.set(0, 0, -0.2)
+  controls.target.set(0, -.5, 0)
   controls.update()
 
   window.addEventListener('resize', onWindowResize)
 }
 
 function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight
+  camera.aspect = 3 / 2
   camera.updateProjectionMatrix()
 
-  renderer.setSize(window.innerWidth, window.innerHeight)
+  if (window.innerWidth <= 480) {
+    controls.target.set(0, .25, 0)
+    controls.update()
+  }
+
+  renderer.setSize(window.innerWidth, window.innerWidth * 0.67)
 
   render()
 }
 
-function render() {
+function render(time) {
+  if (!isTouch) {
+    time *= 0.0001
+
+    if (model && model.rotation) {
+      model.rotation.y = time
+    }
+  }
+  
   renderer.render(scene, camera)
+  actionID = requestAnimationFrame(render)
 }
 
 onMounted(() => {
   init()
+
+  canvas.addEventListener('touchstart', (e) => {
+    isTouch = true
+    cancelAnimationFrame(actionID)
+  })
+
+  canvas.addEventListener('touchmove', (e) => {
+    isTouch = true
+    cancelAnimationFrame(actionID)
+  })
+
+  canvas.addEventListener('touchend', () => {
+    isTouch = false
+    requestAnimationFrame(render)
+  })
 })
 
 onUnmounted(() => {
@@ -77,4 +111,8 @@ onUnmounted(() => {
 })
 </script>
 
-<template></template>
+<template>
+  <main>
+    <canvas id="card"></canvas>
+  </main>
+</template>
