88c2776dd9ba7ce391b23a94ac0cea964d889767
JonPulat-099 <rjpm023@gmail.com>
2024-06-03 19:12:13 +0500

    change: backgroundcolor to transparent

--- src/App.vue
+++ src/App.vue
@@ -14,30 +14,36 @@ let camera,
   canvas,
   model,
   isTouch = false,
+  actionID,
+  progressBarDiv,
   positionY,
-  actionID
+  pauseTime
 
 function init() {
   canvas = document.getElementById('card')
+  canvas.style.backgroundColor = 'transparent'
 
-  camera = new THREE.PerspectiveCamera(55, 3 / 2, 0.25, 20)
-  camera.position.set(0, 0, 2)
+  progressBarDiv = document.querySelector('.loader')
+  progressBarDiv.style.display = 'block'
+  progressBarDiv.style.top = setPosition().progressBarPosition
+  progressBarDiv.style.width = '100%'
+
+  camera = new THREE.PerspectiveCamera(setPosition().fov, 3 / 2, 0.25, 200)
+  camera.position.set(...setPosition().cameraPosition)
 
   scene = new THREE.Scene()
 
   new GLTFLoader().setPath('models/v2/').load('02.gltf', async function (gltf) {
     model = gltf.scene
-
-    // wait until the model can be added to the scene without blocking due to shader compilation
-
     await renderer.compileAsync(model, camera, scene)
 
     scene.add(model)
 
     render()
+    progressBarDiv.style.display = 'none'
   })
 
-  renderer = new THREE.WebGLRenderer({ antialias: true, canvas })
+  renderer = new THREE.WebGLRenderer({ antialias: true, canvas, alpha: true })
   renderer.setPixelRatio(window.devicePixelRatio)
   renderer.setSize(window.innerWidth, window.innerWidth * 0.67)
   renderer.toneMapping = THREE.ACESFilmicToneMapping
@@ -46,14 +52,13 @@ function init() {
   const environment = new RoomEnvironment(renderer)
   const pmremGenerator = new THREE.PMREMGenerator(renderer)
 
-  scene.background = new THREE.Color(0xbbbbbb)
   scene.environment = pmremGenerator.fromScene(environment).texture
 
   controls = new OrbitControls(camera, canvas)
   controls.addEventListener('change', render) // use if there is no animation loop
   controls.minDistance = 3
   controls.maxDistance = 3
-  controls.target.set(0, -.5, 0)
+  controls.target.set(...setPosition().targetPostion)
   controls.update()
 
   window.addEventListener('resize', onWindowResize)
@@ -61,27 +66,50 @@ function init() {
 
 function onWindowResize() {
   camera.aspect = 3 / 2
+  camera.fov = setPosition().fov
+  camera.position.set(...setPosition().cameraPosition)
   camera.updateProjectionMatrix()
 
-  if (window.innerWidth <= 480) {
-    controls.target.set(0, .25, 0)
-    controls.update()
-  }
+  controls.target.set(...setPosition().targetPostion)
+  controls.update()
 
   renderer.setSize(window.innerWidth, window.innerWidth * 0.67)
 
   render()
 }
 
+function setPosition() {
+  var fov, cameraPosition, targetPostion, progressBarPosition
+
+  if (window.innerWidth < 1600) {
+    fov = 38
+    cameraPosition = [1.4, 0, 2]
+    targetPostion = [0, 0, 0]
+    progressBarPosition = (window.innerWidth * 0.67) / 2 - 50 + 'px'
+  } else {
+    fov = 60
+    cameraPosition = [1.4, 0, 2]
+    targetPostion = [0, -0.5, 0]
+    progressBarPosition = window.innerHeight / 2 + 'px'
+  }
+
+  return {
+    fov,
+    cameraPosition,
+    targetPostion,
+    progressBarPosition
+  }
+}
+
 function render(time) {
-  if (!isTouch) {
-    time *= 0.0001
+  if (!isTouch && progressBarDiv.style.display == 'none') {
+    time *= 0.00055
 
     if (model && model.rotation) {
       model.rotation.y = time
     }
   }
-  
+
   renderer.render(scene, camera)
   actionID = requestAnimationFrame(render)
 }
@@ -112,7 +140,8 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <main>
-    <canvas id="card"></canvas>
-  </main>
+  <div class="loader">
+    <div class="custom-loader"></div>
+  </div>
+  <canvas id="card"></canvas>
 </template>
